e149d6b0a7680b0f014cb9ef2672597d09316fd6
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-15 19:12:20 -0300

    git

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import { CreateProductController } from "../controller/CreateProductController";
 
 const router = Router()
+
+const createProduct = new CreateProductController()
+
 
 
 
