201c79fea787651d1abd3a56bb405e443d082175
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-15 23:09:59 -0300

    orm

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateProductController } from "../controller/CreateProductController";
+import { CreateCategoryController } from "../controller/CreateeCategoryController"
 
 const router = Router()
 
 const createProduct = new CreateProductController()
+const createCategory = new CreateCategoryController()
 
 router.post("/product", createProduct.handle)
+router.post("/category", createCategory.handle2)
 
 
 export { router }
