37dd62eabb18b59e9bac4dcd84fe56492eed4603
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-17 10:56:29 -0300

    prisma

---
+++ src/controller/CreateProductWithExistCategory.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { prismaClient } from "../database/prismaClient";
+
+export class CreateProductWithExistCategory{
+    async handle4(req: Request, res: Response){
+        const { name, price, bar_code, id_category } = req.body
+
+        const product2 = await prismaClient.productCategory.create({
+            data:{
+                product:{
+                    create:{
+                        bar_code,
+                        name,
+                        price
+                    }
+                },
+                    category:{
+                        connect:{
+                            id: id_category
+                    }
+                }
+                
+            }
+        })
+
+        return res.json(product2)
+    }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,16 +2,20 @@ import { Router } from "express";
 import { CreateProductController } from "../controller/CreateProductController";
 import { CreateCategoryController } from "../controller/CreateeCategoryController"
 import { CreateProductCategoryController } from "../controller/CreateProductCategoryController";
+import { CreateProductWithExistCategory } from "../controller/CreateProductWithExistCategory"
+
 
 const router = Router()
 
 const createProduct = new CreateProductController()
 const createCategory = new CreateCategoryController()
 const createProductCategoryController = new CreateProductCategoryController()
+const createProductWithExistCategory = new CreateProductWithExistCategory()
 
 router.post("/product", createProduct.handle)
 router.post("/category", createCategory.handle2)
 router.post("/categoryProduct", createProductCategoryController.handle3)
+router.post("/productWithCategoty", createProductWithExistCategory.handle4)
 
 
 export { router }
