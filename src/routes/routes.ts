fb50de952036d7933479b5e18a99adb9a8743a5f
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-17 11:22:44 -0300

    prisma

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,8 @@ import { CreateProductController } from "../controller/CreateProductController";
 import { CreateCategoryController } from "../controller/CreateeCategoryController"
 import { CreateProductCategoryController } from "../controller/CreateProductCategoryController";
 import { CreateProductWithExistCategory } from "../controller/CreateProductWithExistCategory"
+import { FindProductController } from "../controller/FindProductController"
+import { FindCategoryConctroller } from "../controller/FindCategoryController"
 
 
 const router = Router()
@@ -11,11 +13,16 @@ const createProduct = new CreateProductController()
 const createCategory = new CreateCategoryController()
 const createProductCategoryController = new CreateProductCategoryController()
 const createProductWithExistCategory = new CreateProductWithExistCategory()
+const findProductController = new FindProductController()
+const findCategoryConctroller = new FindCategoryConctroller()
+
 
 router.post("/product", createProduct.handle)
 router.post("/category", createCategory.handle2)
 router.post("/categoryProduct", createProductCategoryController.handle3)
 router.post("/productWithCategoty", createProductWithExistCategory.handle4)
+router.get("/product/:id", findProductController.handle)
+router.get("/category/:id", findCategoryConctroller.handle)
 
 
 export { router }
